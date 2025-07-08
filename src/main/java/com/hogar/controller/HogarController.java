34297d1289715dc54f9d6ddb1fd7b1f309287278
Tyler MM <tyler14cr@gmail.com>
2025-07-07 20:07:04 -0600

    Tyler - Se agrega head, header, footer y Menú

--- src/main/java/com/hogar/controller/HogarController.java
+++ src/main/java/com/hogar/controller/HogarController.java
@@ -1,13 +1,15 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.hogar.controller;
 
-/**
- *
- * @author tyler
- */
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class HogarController {
-    
+
+@GetMapping("/menu")
+public String mostrarMenu(Model model) {
+    model.addAttribute("activePage", "menu");
+    return "hogar/menu";
+}
 }
